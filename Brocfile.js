6751467b072923c78202af873fb3ce97a653ba57
Alia Borroho <aliaborroho@gmail.com>
2015-07-12 15:37:12 -0500

    starting to really build my photo site..

--- Brocfile.js
+++ Brocfile.js
@@ -1,6 +1,8 @@
 var merge = require('broccoli-merge-trees');
 var compileSass = require('broccoli-sass');
 
+ var includepaths = [ 'assets', 'bower_components', 'bower_components/fontawesome' 'bower_components/neat/app/assets/stylesheets' 'bower_components/app/assets/stylesheets' ]
+
 var appCss = compileSass(['assets', 'bower_components'], 'app.scss', 'app.css');
 
 module.exports = merge(['public', 'bower_components', appCss]);
