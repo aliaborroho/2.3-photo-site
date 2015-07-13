026a89e9e2dc6a2141721baf41e38df06387e892
Alia Borroho <aliaborroho@gmail.com>
2015-07-12 20:51:14 -0500

    Finally have columns set up , brass tacks complete

--- Brocfile.js
+++ Brocfile.js
@@ -1,8 +1,14 @@
 var merge = require('broccoli-merge-trees');
 var compileSass = require('broccoli-sass');
 
- var includepaths = [ 'assets', 'bower_components', 'bower_components/fontawesome' 'bower_components/neat/app/assets/stylesheets' 'bower_components/app/assets/stylesheets' ]
+var includepaths = [
+	'assets',
+	'bower_components/reset-css',
+	'bower_components/fontawesome/scss',
+	'bower_components/bourbon/app/assets/stylesheets',
+	'bower_components/neat/app/assets/stylesheets'
+];
 
-var appCss = compileSass(['assets', 'bower_components'], 'app.scss', 'app.css');
+var appCss = compileSass(includepaths, 'app.scss', 'app.css');
 
-module.exports = merge(['public', 'bower_components', appCss]);
+module.exports = merge(['public', appCss]);
